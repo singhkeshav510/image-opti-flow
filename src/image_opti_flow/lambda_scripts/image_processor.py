32f7a59874f28247b5f7a8d2ad1aa32118841058
Keshav Singh <keshavsingh@keshavs-macbook-air.home>
2023-09-14 23:07:18 +0200

    test pipeline

--- src/image_opti_flow/lambda_scripts/image_processor.py
+++ src/image_opti_flow/lambda_scripts/image_processor.py
@@ -1,9 +1,11 @@
 import logging
 import os
 
-def image_processor(event: str, context: int) -> str:
+def lambda_handler(event: str, context: int) -> str:
     if type(context) != int:
         return None
+    
+    print(4)
     return event + str(context)
 
 
