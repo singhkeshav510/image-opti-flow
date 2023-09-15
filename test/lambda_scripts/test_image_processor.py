cc71904b73c74a50f22b07a851cc4422c7a5dfd1
Keshav Singh <keshavsingh@keshavs-macbook-air.home>
2023-09-15 02:23:12 +0200

    Added Lambda Code

--- test/lambda_scripts/test_image_processor.py
+++ test/lambda_scripts/test_image_processor.py
@@ -1,15 +1,46 @@
-import pytest
+import unittest
+from unittest.mock import MagicMock, patch
+import os
+import boto3
 
-from src.image_opti_flow.lambda_scripts.image_processor import image_processor
+class TestLambdaFunction(unittest.TestCase):
 
-@pytest.fixture
-def event() -> str:
-    return "Go Pro "
+    @patch('boto3.client')
+    def test_lambda_handler(self, mock_boto3_client):
+        # Mock S3 client and object
+        s3_mock = MagicMock()
+        s3_mock.get_object.return_value = {
+            'Body': MagicMock(read=MagicMock(return_value=b'fake_image_data'))
+        }
+        s3_mock.put_object.return_value = {}
 
-@pytest.mark.parametrize("context", [1, 2, "bro"])
-def test_image_processor_method(event: str, context: int) -> None:
-    result = image_processor(event, context)
-    if type(context) == str:
-        assert result == None
-    else:
-        assert result == event + str(context)
+        # Inject the mocked S3 client
+        mock_boto3_client.return_value = s3_mock
+
+        # Import the Lambda function after patching
+        from src.image_opti_flow.lambda_scripts.image_processor import lambda_handler
+
+        # Define a sample event
+        event = {
+            'key': 'sample_image.jpg'
+        }
+
+        # Set environment variables
+        os.environ['SOURCE_BUCKET'] = 'source-bucket'
+        os.environ['DESTINATION_BUCKET'] = 'destination-bucket'
+
+        # Call the Lambda function
+        result = lambda_handler(event, None)
+
+        # Assertions
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['body'], 'Image processed and uploaded successfully.')
+        s3_mock.get_object.assert_called_once_with(Bucket='source-bucket', Key='sample_image.jpg')
+        s3_mock.put_object.assert_called_once_with(
+            Bucket='destination-bucket',
+            Key='processed/sample_image.jpg',
+            Body=b'fake_image_data'
+        )
+
+if __name__ == '__main__':
+    unittest.main()
